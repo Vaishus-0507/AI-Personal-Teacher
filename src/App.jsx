bbc849d1c81faa925aa0d426b02862597854ff0c
vaishnavi-S02 <sssvaishnavi16@gmail.com>
2026-09-11 20:12:57 +0530

    Update Cloudflare webhook URL

--- src/App.jsx
+++ src/App.jsx
@@ -222,7 +222,7 @@ function handleMarkChange(e) {
 
     try {
       const response = await fetch(
-        'https://monday-counted-vice-pushing.trycloudflare.com/webhook/61244675-6ae6-42aa-9228-60ba21ddf6ad',
+        'https://uri-appeals-raise-assure.trycloudflare.com/webhook/61244675-6ae6-42aa-9228-60ba21ddf6adS',
         {
           method: 'POST',
           headers: {
