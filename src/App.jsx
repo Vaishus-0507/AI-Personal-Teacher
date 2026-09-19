23d666527c8ed13e5510f2c5caee5424d1c9fe25
vaishnavi-S02 <sssvaishnavi16@gmail.com>
2026-09-19 12:46:39 +0530

    Update Cloudflare tunnel URL

--- src/App.jsx
+++ src/App.jsx
@@ -235,7 +235,7 @@ function handleMarkChange(e) {
 
     try {
       const response = await fetch(
-        'https://fate-programmes-spoken-finals.trycloudflare.com/webhook/61244675-6ae6-42aa-9228-60ba21ddf6ad',
+        'https://ordinance-spokesman-victor-congressional.trycloudflare.com/webhook/61244675-6ae6-42aa-9228-60ba21ddf6ad',
         {
           method: 'POST',
           headers: {
